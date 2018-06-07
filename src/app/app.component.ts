f6b96066e66fab50fa961aeb11ce43e15e454906
Ismail Bohri <ismail.bohri@waveinfotech.com>
2018-06-07 10:38:08 +0530

    a

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  /**
+   *
+   */
+  constructor(private apollo: Apollo) {
+   
+  }
+}
